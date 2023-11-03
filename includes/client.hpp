1865e69484024e6cf4f470e892c46faa4f774796
porgito <86039875+porgito@users.noreply.github.com>
2023-11-03 22:32:43 +0100

    Update client.hpp

--- includes/client.hpp
+++ includes/client.hpp
@@ -8,30 +8,28 @@ class Server;
 class Client
 {
 private:
-    int             client_fd;
-    std::string     readbuf;
-    std::string     sendbuf;
-    std::string     nickname;
-    std::string     username;
-    bool            to_deconnect;
-    bool			connexion_password;
-	bool			registrationDone;
-	bool			welcomeSent;
-	bool			hasAllInfo;
-	int				nbInfo;
+   	int             client_fd;
+    	std::string     readbuf;
+    	std::string     sendbuf;
+    	std::string     nickname;
+    	std::string     username;
+	bool            to_deconnect;
+	bool		connexion_password;
+	bool		registrationDone;
+	bool		welcomeSent;
     
 public:
-    Client(int client_fd);
-    ~Client();
+    	Client(int client_fd);
+    	~Client();
 	std::string&	getReadBuffer();
-    std::string&	getSendBuffer();
-    std::string&	getNickname();
-    std::string 	getUsername() const;
-    void            setReadBuffer(std::string const &buf);
-    void	        setSendBuffer(std::string const &buf);
-    bool&			getConnexionPassword();
-    bool&			isRegistrationDone();
-    bool&			reg();
+	std::string&	getSendBuffer();
+    	std::string&	getNickname();
+    	std::string 	getUsername() const;
+    	void            setReadBuffer(std::string const &buf);
+    	void	        setSendBuffer(std::string const &buf);
+   	bool&		getConnexionPassword();
+   	bool&		isRegistrationDone();
+   	bool&		reg();
 };
 
 Client*		getClient(Server *server, int const client_fd);
